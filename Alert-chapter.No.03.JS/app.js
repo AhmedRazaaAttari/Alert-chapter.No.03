c78afa8041212b95243af434eeecec1fef47417e
AhmedRazaaAttari <154536166+AhmedRazaaAttari@users.noreply.github.com>
2024-03-02 13:55:08 +0500

    Add files via upload

---
+++ Alert-chapter.No.03.JS/app.js
@@ -0,0 +1,27 @@
+//  chapter-No-03
+//  1. Declare a variable called age & assign to it your age. Show
+// your age in an alert box.
+
+    alert("I am 21 years old")
+
+// 2. Declare & initialize a variable to keep track of how many
+// times a visitor has visited a web page. Show his/her
+// number of visits on your web page. For example: “You
+// have visited this site N times”.
+
+    alert("You have visited this site 10 times.")
+
+// 3. Declare a variable called birthYear & assign to it your
+// birth year. Show the following message in your browser:
+
+    document.write("My birth year is 2002.<br/>")
+
+// 4. A visitor visits an online clothing store
+// www.xyzClothing.com . Write a script to store in variables
+// the following information:
+// a. Visitor’s name
+// b. Product title
+// c. Quantity i.e. how many products a visitor wants to
+// order
+
+    document.write("Ahmed raza ordered 8 T-shirt(M) on SABALON Clothing store.")
